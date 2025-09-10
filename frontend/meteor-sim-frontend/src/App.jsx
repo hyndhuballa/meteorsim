f40afaf1c0bb23c32b2be83fb590e2ec115ad9b2
hyndhuballa <hyndhuballa@gmail.com>
2025-09-10 23:09:44 +0530

    Initial commit

---
+++ frontend/meteor-sim-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SimulationMap from "./SimulationMap";
+
+function App() {
+  return (
+    <div>
+      <h1>Meteor Impact Simulator 🌍☄️</h1>
+      <SimulationMap />
+    </div>
+  );
+}
+
+export default App;
